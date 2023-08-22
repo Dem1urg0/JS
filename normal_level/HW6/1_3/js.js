8925316a4b622cfdbb18bb060922fb2debb7d2ff
Dem1urg0 <110934892+Dem1urg0@users.noreply.github.com>
2023-08-22 19:08:24 +0200

    N_HW6.1

---
+++ normal_level/HW6/1_3/js.js
@@ -0,0 +1,171 @@
+"use strict";
+/*
+1. Доработать функцию замены картинки в галерее таким образом, чтобы она проверяла наличие большой
+картинки по указанному в src адресу. Если такой картинки не существует или она не доступна, то должна
+ставиться картинка-заглушка сообщающая об ошибке.
+
+3*. Добавить в галерею функцию перехода к следующему изображению. По сторонам от большой картинки
+должны быть стрелки “вперед” и “назад”, по нажатию на которые происходит замена изображения на
+следующее или предыдущее.
+ */
+/**
+ * @property {Object} settings Объект с настройками галереи.
+ * @property {string} settings.previewSelector Селектор обертки для миниатюр галереи.
+ * @property {string} settings.openedImageWrapperClass Класс для обертки открытой картинки.
+ * @property {string} settings.openedImageClass Класс открытой картинки.
+ * @property {string} settings.openedImageScreenClass Класс для ширмы открытой картинки.
+ * @property {string} settings.openedImageCloseBtnClass Класс для картинки кнопки закрыть.
+ * @property {string} settings.openedImageCloseBtnSrc Путь до картинки кнопки открыть.
+ */
+const gallery = {
+    settings: {
+        previewSelector: '.mySuperGallery',
+        openedImageWrapperClass: 'galleryWrapper',
+        openedImageClass: 'galleryWrapper__image',
+        openedImageScreenClass: 'galleryWrapper__screen',
+        openedImageCloseBtnClass: 'galleryWrapper__close',
+        openedImageCloseBtnSrc: 'images/gallery/close.png',
+        arrowLeftClass: 'arrow_left',
+        arrowRightClass: 'arrow_right',
+        arrowLeftSrc: 'images/arrows/left.png',
+        arrowRightSrc: 'images/arrows/right.png',
+    },
+
+    /**
+     * Инициализирует галерею, ставит обработчик события.
+     * @param {Object} userSettings Объект настроек для галереи.
+     */
+    init(userSettings = {}) {
+        // Записываем настройки, которые передал пользователь в наши настройки.
+        Object.assign(this.settings, userSettings);
+
+        // Находим элемент, где будут превью картинок и ставим обработчик на этот элемент,
+        // при клике на этот элемент вызовем функцию containerClickHandler в нашем объекте
+        // gallery и передадим туда событие MouseEvent, которое случилось.
+        document
+            .querySelector(this.settings.previewSelector)
+            .addEventListener('click', event => this.containerClickHandler(event));
+    },
+
+    /**
+     * Обработчик события клика для открытия картинки.
+     * @param {MouseEvent} event Событие клики мышью.
+     * @param {HTMLElement} event.target Целевой объект, куда был произведен клик.
+     */
+    containerClickHandler(event) {
+        // Если целевой тег не был картинкой, то ничего не делаем, просто завершаем функцию.
+        if (event.target.tagName !== 'IMG') {
+            return;
+        }
+        // Открываем картинку с полученным из целевого тега (data-full_image_url аттрибут).
+        this.openImage(event.target.dataset.full_image_url);
+    },
+
+    /**
+     * Открывает картинку.
+     * @param {string} src Ссылка на картинку, которую надо открыть.
+     */
+    openImage(src) {
+        // Получаем контейнер для открытой картинки, в нем находим тег img и ставим ему нужный src.
+        let image = this.getScreenContainer().querySelector(`.${this.settings.openedImageClass}`)
+        image.onerror = function () {
+            image.src = 'images/404.png';
+        };
+        image.src = src;
+    },
+
+    /**
+     * Возвращает контейнер для открытой картинки, либо создает такой контейнер, если его еще нет.
+     * @returns {Element}
+     */
+    getScreenContainer() {
+        // Получаем контейнер для открытой картинки.
+        const galleryWrapperElement = document.querySelector(`.${this.settings.openedImageWrapperClass}`);
+        // Если контейнер для открытой картинки существует - возвращаем его.
+        if (galleryWrapperElement) {
+            return galleryWrapperElement;
+        }
+        // Возвращаем полученный из метода createScreenContainer контейнер.
+        return this.createScreenContainer();
+    },
+
+    /**
+     * Создает контейнер для открытой картинки.
+     * @returns {HTMLElement}
+     */
+    createScreenContainer() {
+        // Создаем сам контейнер-обертку и ставим ему класс.
+        const galleryWrapperElement = document.createElement('div');
+        galleryWrapperElement.classList.add(this.settings.openedImageWrapperClass);
+
+        // Создаем контейнер занавеса, ставим ему класс и добавляем в контейнер-обертку.
+        const galleryScreenElement = document.createElement('div');
+        galleryScreenElement.classList.add(this.settings.openedImageScreenClass);
+        galleryWrapperElement.appendChild(galleryScreenElement);
+
+        // Создаем картинку для кнопки закрыть, ставим класс, src и добавляем ее в контейнер-обертку.
+        const closeImageElement = new Image();
+        closeImageElement.classList.add(this.settings.openedImageCloseBtnClass);
+        closeImageElement.src = this.settings.openedImageCloseBtnSrc;
+        closeImageElement.addEventListener('click', () => this.close());
+        galleryWrapperElement.appendChild(closeImageElement);
+        // Стрелки для перелистывания
+        const arrowLeft = new Image();
+        arrowLeft.classList.add(this.settings.arrowLeftClass);
+        arrowLeft.src = this.settings.arrowLeftSrc;
+        arrowLeft.addEventListener('click', () => this.left());
+        galleryWrapperElement.appendChild(arrowLeft);
+
+        const arrowRight = new Image();
+        arrowRight.classList.add(this.settings.arrowRightClass);
+        arrowRight.src = this.settings.arrowRightSrc;
+        arrowRight.addEventListener('click', () => this.right());
+        galleryWrapperElement.appendChild(arrowRight);
+
+        // Создаем картинку, которую хотим открыть, ставим класс и добавляем ее в контейнер-обертку.
+        const image = new Image();
+        image.classList.add(this.settings.openedImageClass);
+        galleryWrapperElement.appendChild(image);
+
+        // Добавляем контейнер-обертку в тег body.
+        document.body.appendChild(galleryWrapperElement);
+
+        // Возвращаем добавленный в body элемент, наш контейнер-обертку.
+        return galleryWrapperElement;
+    },
+
+    /**
+     * Закрывает (удаляет) контейнер для открытой картинки.
+     */
+    close() {
+        document.querySelector(`.${this.settings.openedImageWrapperClass}`).remove();
+    },
+    /**
+     * Перелистывает картинку в лево и право
+     */
+    left() {
+        const img = document.querySelector(`.${this.settings.openedImageWrapperClass}`)
+            .querySelector(`.${this.settings.openedImageClass}`);
+        const src = img.src;
+        const filename = src.substring(src.lastIndexOf('/') + 1);
+        const name = parseInt(filename.split('.')[0]);
+        if (name == 1) {
+            img.src = `images/max/4.jpg`
+        } else
+            img.src = `images/max/${name - 1}.jpg`
+    },
+    right() {
+        const img = document.querySelector(`.${this.settings.openedImageWrapperClass}`)
+            .querySelector(`.${this.settings.openedImageClass}`);
+        const src = img.src;
+        const filename = src.substring(src.lastIndexOf('/') + 1);
+        const name = parseInt(filename.split('.')[0]);
+        if (name == 4) {
+            img.src = `images/max/1.jpg`
+        } else
+            img.src = `images/max/${name + 1}.jpg`
+    }
+};
+
+// Инициализируем нашу галерею при загрузке страницы.
+window.onload = () => gallery.init({previewSelector: '.galleryPreviewsContainer'});
