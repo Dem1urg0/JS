d0c1e6ab740362c0df62a58645c649ec0c758d9e
Dem1urg0 <110934892+Dem1urg0@users.noreply.github.com>
2023-10-11 15:16:11 +0200

    adw_HW2

--- adw_level/HW1/js/main.js
+++ adw_level/HW1/js/main.js
@@ -3,7 +3,7 @@
 2. Добавьте значения по умолчанию для аргументов функции. Как можно упростить или
 сократить запись функций?
 
-Ответ: Убрал лишнее равенство у второй функции.
+Ответ: В первой функции убрал return и убрал лишнее равенство у второй функции.
 Как было:
 const productList = list.map(item => renderProduct(item.title, item.price));
 document.querySelector('.products').innerHTML = productList;
@@ -22,16 +22,16 @@ const products = [
   {id: 4, title: 'Gamepad', price: 150},
 ];
 
-const renderProduct = (title = 'Пусто', price = 150) => {
-  return `<div class="products_item">
+const renderProduct = (title = 'Пусто', price = 150) =>
+    `<div class="products_item">
             <h3>${title}</h3>
             <p>${price}</p>
             <button class="by-btn">Добавить</button>
-          </div>`;
-};
+          </div>`
 
 const renderProducts = list => {
-  document.querySelector('.products').innerHTML = list.map(item => renderProduct(item.title, item.price)).join("");
+  document.querySelector('.products')
+      .innerHTML = list.map(item => renderProduct(item.title, item.price)).join("");
 };
 
 renderProducts(products);
