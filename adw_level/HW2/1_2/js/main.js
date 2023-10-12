a52dd67993677a126ec5df5a0d89c25413d91d76
Dem1urg0 <110934892+Dem1urg0@users.noreply.github.com>
2023-10-12 14:36:01 +0200

    adw_HW2.1
    Исправил проектный сайт (стили, название классов)

--- adw_level/HW2/1_2/js/main.js
+++ adw_level/HW2/1_2/js/main.js
@@ -1,47 +1,42 @@
-/*
-1. Добавьте пустые классы для Корзины товаров и Элемента корзины товаров. Продумайте,
-какие методы понадобятся для работы с этими сущностями.
-
-2. Добавьте для GoodsList метод, определяющий суммарную стоимость всех товаров.
-
-*/
-class ProductList {
-  constructor(container = '.products') {
-    this.container = container;
-    this.goods = [];
-    this.allProducts = [];
-    this._fetchProducts();
-    this._render();
-  }
-  _fetchProducts() {
-    this.goods = [
-      {id: 1, title: 'Notebook', price: 1000},
-      {id: 2, title: 'Mouse', price: 100},
-      {id: 3, title: 'Keyboard', price: 250},
-      {id: 4, title: 'Gamepad', price: 150},
-    ]
-  }
-  _render() {
-    const block = document.querySelector(this.container);
-
-    for (let product of this.goods) {
-      const productObject = new ProductItem(product);
-      this.allProducts.push(productObject);
-      block.insertAdjacentHTML('beforeend', productObject.render())
+class GoodsList {
+    constructor(container = '.goods') {
+        this.container = container;
+        this.goods = [];
+        this.allGoods = [];
+        this._fetchGoods();
+        this._render();
     }
-  }
+
+    _fetchGoods() {
+        this.goods = [
+            {id: 1, title: 'Notebook', price: 1000},
+            {id: 2, title: 'Mouse', price: 100},
+            {id: 3, title: 'Keyboard', price: 250},
+            {id: 4, title: 'Gamepad', price: 150},
+        ]
+    }
+
+    _render() {
+        const block = document.querySelector(this.container);
+
+        for (let good of this.goods) {
+            const goodsObject = new GoodItem(good);
+            this.allGoods.push(goodsObject);
+            block.insertAdjacentHTML('beforeend', goodsObject.render())
+        }
+    }
 }
 
-class ProductItem {
-  constructor(product, img = 'https://placehold.it/200x150') {
-    this.title = product.title;
-    this.price = product.price;
-    this.id = product.id;
-    this.img = img;
-  }
+class GoodItem {
+    constructor(good, img = '') {
+        this.title = good.title;
+        this.price = good.price;
+        this.id = good.id;
+        this.img = img;
+    }
 
-  render() {
-    return `<div class="product-item" data-id="${this.id}">
+    render() {
+        return `<div class="good-item" data-id="${this.id}">
                 <img src="${this.img}" alt="Some img">
                 <div class="desc">
                     <h3>${this.title}</h3>
@@ -49,26 +44,8 @@ class ProductItem {
                     <button class="buy-btn">Купить</button>
                 </div>
             </div>`;
-  }
+    }
 }
 
-const list = new ProductList();
+const list = new GoodsList();
 
-// const products = [
-//   {id: 1, title: 'Notebook', price: 1000},
-//   {id: 2, title: 'Mouse', price: 100},
-//   {id: 3, title: 'Keyboard', price: 250},
-//   {id: 4, title: 'Gamepad', price: 150},
-// ];
-//
-// const renderProduct = (item, img = 'https://placehold.it/200x150') => `<div class="product-item">
-//             <img src="${img}" alt="Some img">
-//             <h3>${item.title}</h3>
-//             <p>${item.price}</p>
-//             <button class="by-btn">Добавить</button>
-//           </div>`;
-//
-// const renderProducts = list => document.querySelector('.products')
-//   .insertAdjacentHTML('beforeend', list.map(item => renderProduct(item)).join(''));
-//
-// renderProducts(products);
