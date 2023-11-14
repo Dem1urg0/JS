6a88e9a9520a7b75e73d78a682c0193c76169cb5
main <arte.graboveckii@gmail.com>
2023-11-14 17:42:22 +0300

    adw_HW5
    Подготовил проект для работы с заданиями

---
+++ adw_level/HW5/js/main.js
@@ -0,0 +1,37 @@
+/*
+1. Добавить методы и обработчики событий для поля поиска. Создать в объекте данных поле searchLine и привязать к нему содержимое поля ввода.
+На кнопку Искать добавить обработчик клика, вызывающий метод FilterGoods.
+2. Добавить корзину. В html-шаблон добавить разметку корзины. Добавить в объект данных поле isVisibleCart, управляющее видимостью корзины.
+3. * Добавлять в .goods-list заглушку с текстом «Нет данных» в случае, если список товаров пуст.
+ */
+
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    catalogUrl: '/catalogData.json',
+    products: [],
+    imgCatalog: 'https://placehold.it/200x150'
+  },
+  methods: {
+    getJson(url){
+      return fetch(url)
+        .then(result => result.json())
+        .catch(error => {
+          console.log(error);
+        })
+    },
+    addProduct(product){
+      console.log(product.id_product);
+    }
+  },
+  mounted(){
+    this.getJson(`${API + this.catalogUrl}`)
+      .then(data => {
+        for(let el of data){
+          this.products.push(el);
+        }
+      });
+  }
+});
